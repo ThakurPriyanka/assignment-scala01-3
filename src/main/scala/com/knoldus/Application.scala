3e3cc668343644e9ada393d50d2a51580babbe92
ThakurPriyanka <aroma.priyanka@gmail.com>
2018-01-10 18:07:10 +0530

    Initial commit

---
+++ src/main/scala/com/knoldus/Application.scala
@@ -0,0 +1,32 @@
+package com.knoldus
+
+object Application {
+  def main(args: Array[String]): Unit = {
+    val num1 = 1
+    val num2 = 2
+    val num3 = 4
+    val num4 = 6
+    val num5 = 8
+    val num6 = 1
+    val num7 = 2
+    val num8 = 4
+    val num9 = 6
+    val num0 = 8
+    val list1 = List(num1, num2, num3, num4, num5)
+    val list2 = List(num6, num7, num8, num9, num0)
+    val operationObject = new Operations
+    print(s"Sum of two list ${operationObject.addingTwoList(list1,list2)} \n")
+
+    val shape = "parallelogram"
+    val areaResult = operationObject.area(shape,num1,num2)
+    print(s"Area $areaResult \n")
+
+    print(s"Doubling the list element  ${operationObject.doublingListElement(list1)} \n")
+    print(s"Number of Element  ${operationObject.numberOfElement(list1)} \n")
+    print(s"kTH Element  ${operationObject.kthElement(list1,num2)} \n")
+
+    print(s"Reverse List  ${operationObject.reverse(list1)} \n")
+
+    print(s"Fibonacci  ${operationObject.fibonacci(num3)} \n")
+  }
+}
---
+++ src/main/scala/com/knoldus/Operations.scala
@@ -0,0 +1,74 @@
+package com.knoldus
+
+class Operations {
+  def doublingListElement(list: List[Int]): List[Int] = {
+    list.map(item => item * 2)
+  }
+
+  def addingTwoList(list1: List[Int], list2: List[Int]): List[Int] = {
+    if (list1.length == list2.length) {
+      val resultList = for (i <- 0 to list1.length - 1) yield list1(i) + list2(i)
+      resultList.to[List]
+    }
+    else {
+      List()
+    }
+  }
+
+  def area(shape: String, first: Int, second: Int): String = {
+    val shapeInput = shape.toLowerCase
+    def areaFinal(num1: Int,num2: Int,f:(Int,Int) => Int): Int ={
+      f(num1,num2)
+    }
+    shapeInput match {
+      case "parallelogram" => {
+        val result = areaFinal(first,second,(first, second)=> 1/2 *  first*second)
+        s"Area of parallelogram is $result"
+      }
+      case "rectangle" | "rhombus" => {
+        val result = areaFinal(first,second,(first, second)=> first*second)
+        s"Area of Rectangle is $result"
+      }
+      case _ => s"Not defined for $shape "
+    }
+  }
+def numberOfElement(list: List[Int]): Int = {
+  def counter(list: List[Int],count: Int): Int ={
+    list match {
+      case head::tail => counter(tail,count + 1)
+      case Nil => count
+    }
+  }
+  val count = 0
+  counter(list,count)
+}
+  def kthElement(list: List[Int], position: Int): Int = {
+    if(position < list.length) {
+      list match {
+        case head :: tail if position > 0 => kthElement(tail, position - 1)
+        case head :: tail if (position == 0) => head
+      }
+    }
+    else {
+      -1
+    }
+  }
+
+  def reverse(list: List[Int]): List[Int] = {
+    val resultList = for(i <- list.length-1 to 0 by -1) yield list(i)
+    resultList.to[List]
+  }
+  def fibonacci( num : Int) : List[Int] = {
+    def fib(num: Int): Int = {
+      def fibTail(num: Int, first: Int, second: Int): Int = num match {
+        case 0 => first
+        case _ => fibTail(num - 1, second, first + second)
+      }
+      fibTail(num, 0, 1)
+    }
+
+  val result =  for (i <- 0 until num) yield  { fib(i) }
+  result.to[List]
+  }
+}
+
